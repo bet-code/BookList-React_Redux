336d2dbbe6a5954543780a35e8286c881c0872ac
Gilbert <gilbert.j5@outlook.com>
2018-01-17 12:53:00 -0500

    added reducer

---
+++ dev/scripts/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        {title: 'JavaScript: The Good Parts'},
+        {title: 'Algorithms to Live By'},
+        {title: 'The Book of Five Rings'},
+        {title: 'The Secret'},
+        {title: 'Big Magic'}
+    ]
+}

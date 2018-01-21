22e3d4ae771995cc23b999ff8e4b66490f3f501e
Gilbert <gilbert.j5@outlook.com>
2018-01-20 20:40:57 -0500

    add component books-lists.js

---
+++ dev/scripts/components/book-list.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class BookList extends Component {
+
+    rederList() {
+        return this.props.books.map((book) => {
+            return (
+                <li key={book.title}className="list-group-item">{book.title}</li>
+            )
+        })
+    }
+
+    render() {
+        return (
+            <ul className="list-group">
+                {this.renderList()}
+            </ul>
+        )
+    }
+}
